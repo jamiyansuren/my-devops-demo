b77656bc443078765d4c6ce004c26fd1b1d35c85
jamiyansuren <bigdata1121@gmail.com>
2025-05-14 14:59:46 +0800

    initial commit

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return "Hello from Docker Compose!@"
+    return "Hello CI/CD! This is a simple Flask application"
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000, debug=True)
